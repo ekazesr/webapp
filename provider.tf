d4ca09e4398624b4d550d90e10eca6bcc2c2fce8
ekazesr <ekazesr@gmail.com>
2024-03-05 22:22:27 +0000

    fixes

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.region # change your region as per your requriements
-  access_key="AKIATG2NJ2OEAZXM2Z7D"
-  secret_key = "ABeI0pubTLhvCccxo/O4CiU3vMN4dt8IyYbdrgbz"
+  # access_key=""
+  # secret_key = ""
 
 }
